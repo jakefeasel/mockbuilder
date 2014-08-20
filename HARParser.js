169abb48c592383df77289284a8d25a714fb3723
jakefeasel <jfeasel@gmail.com>
2014-08-20 13:42:39 -0700

    Updating to be more useful for forgerock mocks - JSON vs. nonJSON types, FR copyright, etc...

--- HARParser.js
+++ HARParser.js
@@ -2,23 +2,45 @@ define( ["handlebars", "lodash"],
     function (Handlebars, _) {
         return {
             parse: function (testTemplate, harData) {             
-                var compiledTemplate = Handlebars.compile(testTemplate);
+                var compiledTemplate = Handlebars.compile(testTemplate),
+                    isXHR = function (e) {
+                        console.log(e.request.url + " isXHR? " + _.find(e.request.headers, function (h) {
+                            return h.name === "X-Requested-With" && h.value === "XMLHttpRequest";
+                        }) || e.request.url.match(/\.less$/))
 
-                harData.log.entries = 
+                        return _.find(e.request.headers, function (h) {
+                            return h.name === "X-Requested-With" && h.value === "XMLHttpRequest";
+                        }) || e.request.url.match(/\.less$/);
+                    },
+                    isJSONResponse = function (e) {
+                        var contentType = _.find(e.response.headers, function (h) {
+                            return h.name === "Content-Type";
+                        });
+
+                        return contentType && contentType.value.match(/^application\/json/);
+                    },
+                    asTextContent = function (e) {
+                        // put the response back in JSON so it will be safe to embed in the Javascript directly
+                        e.response.content.text = JSON.stringify(e.response.content.text);
+                        // add a flag to help with handling trailing commas
+                        e.response.headers[e.response.headers.length-1].last = true;
+
+                        e.request.url = e.request.url.replace(/https?:\/\/.*?\//i, '/');
+                        return e;
+                    };
+
+                harData.log.nonJSONEntries = 
                     _(harData.log.entries)
-                        // only care about the XHR requests
-                        .filter(function (e) {
-                            return _.find(e.request.headers, function (h) {
-                                return h.name === "X-Requested-With" && h.value === "XMLHttpRequest";
-                            });
-                        })
-                        .map(function (e) {
-                            // put the response back in JSON so it will be safe to embed in the Javascript directly
-                            e.response.content.text = JSON.stringify(e.response.content.text);
-                            // add a flag to help with handling trailing commas
-                            e.response.headers[e.response.headers.length-1].last = true;
-                            return e;
-                        })
+                        .filter(isXHR)
+                        .reject(isJSONResponse)
+                        .map(asTextContent)
+                        .value();
+
+                harData.log.JSONEntries = 
+                    _(harData.log.entries)
+                        .filter(isXHR)
+                        .filter(isJSONResponse)
+                        .map(asTextContent)
                         .value();
 
                 return compiledTemplate(harData);
--- sinon_fake_server.hb
+++ sinon_fake_server.hb
@@ -1,16 +1,60 @@
+/**
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2014 ForgeRock AS. All Rights Reserved
+ *
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * http://forgerock.org/license/CDDLv1.0.html
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at http://forgerock.org/license/CDDLv1.0.html
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ */
+
 /*global require, define*/
-define(function () {
+define([{{#each log.nonJSONEntries}}
+    "text!{{{request.url}}}"{{#unless last}},{{/unless}}{{/each}}
+], function () {
+
+    /* an unfortunate need to duplicate the file names here, but I haven't
+     yet found a way to fool requirejs into doing dynamic dependencies */
+    var staticFiles = [{{#each log.nonJSONEntries}}
+            "{{{request.url}}}"{{#unless last}},{{/unless}}{{/each}}
+        ],
+        deps = arguments;
 
     return function (server) {
-{{#each log.entries}}    
+
+        _.each(staticFiles, function (file, i) {
+            server.respondWith(
+                "GET",
+                new RegExp(file.replace(/([\/\.\-])/g, "\\$1") + "$"),
+                [
+                    200,
+                    { },
+                    deps[i]
+                ]
+            );
+        });
+
+{{#each log.JSONEntries}}    
         server.respondWith({{#request}}
             "{{method}}",   
             "{{{url}}}",{{/request}}
             [{{#response}}
                 {{status}}, 
-                { {{#each headers}}
-                    "{{name}}": "{{value}}"{{^last}},{{/last}}{{/each}}
-                },
+                { },
                 {{{content.text}}}
             {{/response}}]
         );
